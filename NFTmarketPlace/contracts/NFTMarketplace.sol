e0761bd02cb06f9438052a8d86c87599ba6a6578
Pradum <satendarpradum@gmail.com>
2025-08-15 11:51:41 +0530

    Added ListItem

--- NFTmarketPlace/contracts/NFTMarketplace.sol
+++ NFTmarketPlace/contracts/NFTMarketplace.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.20;
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -25,5 +25,20 @@ contract NFTMarketplace is ReentrancyGuard, Ownable{
 
         constructor(){}
 
+
+        function listItem(address nftAddress,uint256 tokenId,uint256 price) external nonReentrant{
+            require(price>0,"Price must be greater than zero");
+            IERC721 nft=IERC721(nftAddress);
+
+            //Check that msg.sender owns the nft
+            require(nft.ownerOf(tokenId)==msg.sender,"You are not a Owner of this nft");
+
+            //Check marketPlace approval
+            require(nft.getApproved(tokenId)==address(this),"Market Place not approved");
+
+            listings[nftAddress][tokenId]=Listing(price,msg.sender);
+
+            emit ItemListed(nftAddress, tokenId, msg.sender, price);
+        }
 
 }
